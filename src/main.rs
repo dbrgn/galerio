c0596f52673587cd0ccaba26b3052a70420910a7
Danilo Bargen <mail@dbrgn.ch>
2020-12-06 15:43:23 +0100

    Generate simple flexbox gallery

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::time::Instant;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    time::Instant,
+};
 
 use anyhow::{anyhow, Result};
 use image::{self, imageops::FilterType, ImageFormat};
 use lazy_static::lazy_static;
+use serde::Serialize;
 use structopt::StructOpt;
+use tera::Tera;
 
 const NAME: &str = "gallerist";
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 lazy_static! {
     static ref START_TIME: Instant = Instant::now();
@@ -35,8 +40,29 @@ struct Args {
     /// Output directory
     #[structopt(parse(from_os_str))]
     output_dir: PathBuf,
+
+    /// Gallery title
+    title: String,
+
+    /// Skip creating thumbnails
+    #[structopt(long)]
+    skip_thumbnails: bool,
 }
 
+#[derive(Serialize)]
+struct Image {
+    filename_full: String,
+    filename_thumb: String,
+}
+
+#[derive(Serialize)]
+struct Context {
+    title: String,
+    gallerist_version: &'static str,
+    images: Vec<Image>,
+}
+
+/// Generate a thumbnail from the `image_path`, return the thumbnail bytes.
 fn make_thumbnail(image_path: impl AsRef<Path>, thumbnail_size: u32) -> Result<Vec<u8>> {
     let img = image::open(image_path)?;
     let mut buf = Vec::new();
@@ -87,17 +113,44 @@ fn main() -> Result<()> {
         .collect::<Vec<_>>();
 
     // Process images
+    let mut images = Vec::with_capacity(image_files.len());
     for f in image_files {
-        log!("Processing {:?}", f.file_name().unwrap());
-        let thumbnail_bytes = make_thumbnail(&f, 512)?;
-        let thumbnail_path = args.output_dir.join(&format!(
+        // Determine filenames
+        let filename_full = f.file_name().unwrap().to_str().unwrap().to_string();
+        let filename_thumb = format!(
             "{}.thumb.jpg",
             f.file_stem()
                 .and_then(|stem| stem.to_str())
                 .ok_or_else(|| anyhow!("Could not determine file stem for file {:?}", f))?,
-        ));
-        fs::write(thumbnail_path, thumbnail_bytes)?;
+        );
+
+        // Resize
+        if !args.skip_thumbnails {
+            log!("Processing {:?}", filename_full);
+            let thumbnail_bytes = make_thumbnail(&f, 512)?;
+            let thumbnail_path = args.output_dir.join(&filename_thumb);
+            fs::write(thumbnail_path, thumbnail_bytes)?;
+        }
+
+        // Store
+        images.push(Image {
+            filename_full,
+            filename_thumb,
+        });
     }
+
+    // Create template context
+    let context = Context {
+        title: args.title.clone(),
+        gallerist_version: VERSION,
+        images,
+    };
+
+    // Generate index.html
+    let tera = Tera::new("templates/**/*.html")?;
+    let rendered = tera.render("index.html", &tera::Context::from_serialize(&context)?)?;
+    log!("Writing index.html");
+    fs::write(args.output_dir.join("index.html"), rendered)?;
 
     Ok(())
 }
